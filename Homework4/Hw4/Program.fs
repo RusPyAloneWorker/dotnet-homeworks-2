0b80402afb0750dd3d4fd46b7d1aac97da47f9a3
JosephMonteCarlo <DioBrando2003@yandex.ru>
2022-10-10 12:42:19 +0300

    Program.fs changed

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,11 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4.Parser
+open Hw4.Calculator
+
+
+let args = [|"4"; "+"; "5"|]
+
+let res = parseCalcArguments args 
+
+let d = calculate res.arg1 res.operation res.arg2 
+
+printf "%f" d
